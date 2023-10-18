f8aa72b87aee554961732131046af813f611c4ee
David Pratt <david@midnightmechanism.com>
2023-10-18 17:47:22 -0500

    Add imputation

--- classify.R
+++ classify.R
@@ -6,6 +6,13 @@ library(doParallel)
 library(ROSE)
 library(corrplot)
 library(lubridate)
+library(mice)
+library(VIM)
+library(doParallel)
+
+
+cl <- makeCluster(15)  # Use 4 cores
+registerDoParallel(cl)
 
 pw <- {"password"}
 
@@ -18,11 +25,27 @@ con <- dbConnect(drv,
                  user = "postgres", 
                  password = pw)
 
-testdata = dbGetQuery(con, "SELECT * FROM testdata")
-traindata = dbGetQuery(con, "SELECT * FROM traindata")
+traindata = dbGetQuery(con, "SELECT * FROM traindata") 
+testdata = dbGetQuery(con, "SELECT * FROM testdata") 
 
-head(testdata)
-head(traindata)
+# Evaluate columns 1-84 for missingness in traindata
+for (i in 1:84) {
+    print(sum(is.na(traindata[, i])))
+}
+
+# Evaluate columns 1-84 for missingness in testdata
+for (i in 1:84) { 
+    print(sum(is.na(testdata[, i])))
+}
+
+# impute missing data
+imputed_data <- foreach(i = 1:3) %dopar% {
+  VIM::kNN(traindata, k = 3)
+}
+
+# stop cluser and garbage collect
+stopCluster(cl)
+gc()
 
 # feature engineer data time
 df = traindata
@@ -42,4 +65,24 @@ split_data <- strsplit(data, "-", fixed = TRUE)
 data = df$`Flow ID`
 first_parts <- sapply(split_data, function(x) x[1])
 
+# include ONLY the numeric variables that differ between the classes (bonferroni)
+pvalues = vector()
+indices = vector()
+for(i in 12:84){
+    trojan_data <- traindata[traindata$class == "Trojan", i]
+    benign_data <- traindata[traindata$class == "Benign", i]
 
+    # Perform the Wilcoxon rank-sum test
+    pvalues[i] = wilcox.test(trojan_data, benign_data, na.action = na.omit)$p.value
+    indices[i] = i
+    print(i)
+}
+
+correlations = cor(traindata[, indices[12: 84] ] )
+# Assuming you want to identify pairs with a correlation of at least 0.7
+high_corr_pairs <- which(abs(correlations) >= 0.95 & correlations != 1, arr.ind = TRUE)
+
+# Remove data with constant columns
+# constant_columns <- apply(traindata[, 12:84], 2, var) == 0
+# traindata <- traindata[, -as.numeric(which(constant_columns))]
+# pca_result <- prcomp(traindata[, 12:84], center = TRUE, scale = TRUE)
