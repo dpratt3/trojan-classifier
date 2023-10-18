3865612545e29445587c0a2e03d0290defc752bb
David Pratt <david@midnightmechanism.com>
2023-10-17 21:14:06 -0500

    Begin classification process

--- classify.R
+++ classify.R
@@ -5,6 +5,7 @@ library(mgcv)
 library(doParallel)
 library(ROSE)
 library(corrplot)
+library(lubridate)
 
 pw <- {"password"}
 
@@ -22,5 +23,23 @@ traindata = dbGetQuery(con, "SELECT * FROM traindata")
 
 head(testdata)
 head(traindata)
+
+# feature engineer data time
+df = traindata
+df$Timestamp <- as.POSIXct(df$Timestamp)
+df$day_of_week <- as.factor(wday(df$Timestamp, label = TRUE))
+df$hour_of_day <- as.factor(hour(df$Timestamp))
+
+# Chi-Sq. test for association between times
+chisq.test(df$class, df$day_of_week)
+chisq.test(df$class, df$hour_of_day)
+
+# For IP Address data, take everyting left of the dash
+# Split at the first hyphen
+split_data <- strsplit(data, "-", fixed = TRUE)
+
+# Extract the first part
+data = df$`Flow ID`
+first_parts <- sapply(split_data, function(x) x[1])
 
 
