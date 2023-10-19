3a9538a261fa7c59b954e3ad26e2e28ab5ae7fa4
David Pratt <david@midnightmechanism.com>
2023-10-18 21:27:44 -0500

    Finish PCA of raw data

--- classify.R
+++ classify.R
@@ -8,11 +8,6 @@ library(corrplot)
 library(lubridate)
 library(mice)
 library(VIM)
-library(doParallel)
-
-
-cl <- makeCluster(15)  # Use 4 cores
-registerDoParallel(cl)
 
 pw <- {"password"}
 
@@ -38,37 +33,49 @@ for (i in 1:84) {
     print(sum(is.na(testdata[, i])))
 }
 
-# impute missing data
-imputed_data <- foreach(i = 1:3) %dopar% {
-  VIM::kNN(traindata, k = 3)
-}
+# Create a pre-processing object to impute with medians
+preproc <- preProcess(traindata, method = "medianImpute")
 
-# stop cluser and garbage collect
-stopCluster(cl)
-gc()
+# Apply the pre-processing to your data
+# aggr(traindata, col = c("navy", "red"), numbers = TRUE, sortVars = TRUE)
+preproc <- preProcess(traindata, method = "medianImpute")
+traindata <- predict(preproc, newdata = traindata)
 
-# feature engineer data time
-df = traindata
-df$Timestamp <- as.POSIXct(df$Timestamp)
-df$day_of_week <- as.factor(wday(df$Timestamp, label = TRUE))
-df$hour_of_day <- as.factor(hour(df$Timestamp))
+# feature engineer data time into days and hours
+traindata$Timestamp <- as.POSIXct(traindata$Timestamp)
+traindata$day_of_week <- as.factor(wday(traindata$Timestamp, label = TRUE))
+traindata$hour_of_day <- as.factor(hour(traindata$Timestamp))
+traindata <- traindata[, !names(traindata) %in% "Timestamp"]
+traindata <- traindata[, c("day_of_week", "hour_of_day", names(traindata)[!names(traindata) %in% c("day_of_week", "hour_of_day")])]
 
 # Chi-Sq. test for association between times
-chisq.test(df$class, df$day_of_week)
-chisq.test(df$class, df$hour_of_day)
+chisq.test(traindata$class, traindata$day_of_week)
+chisq.test(traindata$class, traindata$hour_of_day)
 
 # For IP Address data, take everyting left of the dash
-# Split at the first hyphen
+# Split at the first hyphen and Extract the first part
+data = traindata$`Flow ID`
 split_data <- strsplit(data, "-", fixed = TRUE)
+traindata$`Flow ID` <- as.factor(sapply(split_data, function(x) x[1]))
+traindata[, 1:9] = apply(traindata[, 1:9], 2, as.factor)
 
-# Extract the first part
-data = df$`Flow ID`
-first_parts <- sapply(split_data, function(x) x[1])
+cat_pvalues = vector()
+for(i in 1:9){
+    cat_pvalues[i] = (chisq.test(traindata$class, traindata[, i]))$p.value
+}
+
+# Omit nonsignificant columns
+traindata <- traindata[, -which(cat_pvalues > 0.01)]
+
+# remove near-zero variance
+zero_var_cols <- nearZeroVar(traindata)
+traindata = traindata[, -zero_var_cols]
 
 # include ONLY the numeric variables that differ between the classes (bonferroni)
 pvalues = vector()
 indices = vector()
-for(i in 12:84){
+
+for(i in 9:(dim(traindata)[[2]] - 1)){
     trojan_data <- traindata[traindata$class == "Trojan", i]
     benign_data <- traindata[traindata$class == "Benign", i]
 
@@ -78,11 +85,19 @@ for(i in 12:84){
     print(i)
 }
 
-correlations = cor(traindata[, indices[12: 84] ] )
-# Assuming you want to identify pairs with a correlation of at least 0.7
-high_corr_pairs <- which(abs(correlations) >= 0.95 & correlations != 1, arr.ind = TRUE)
+traindata = traindata[, -which(pvalues > 0.01 / length(pvalues))] # bonferroni correction]
 
-# Remove data with constant columns
-# constant_columns <- apply(traindata[, 12:84], 2, var) == 0
-# traindata <- traindata[, -as.numeric(which(constant_columns))]
-# pca_result <- prcomp(traindata[, 12:84], center = TRUE, scale = TRUE)
+correlations = cor(traindata[, indices[9: (dim(traindata)[[2]] - 1) ] ] )
+# Assuming you want to identify pairs with a correlation of at least 0.7
+high_corr_pairs <- which(abs(correlations) >= 0.90 & correlations != 1, arr.ind = TRUE)
+
+print(high_corr_pairs) # just for fun
+
+numeric_pca = prcomp(traindata[, 9:(dim(traindata)[[2]] - 1)], center = TRUE, scale = TRUE)
+cumulative_variance <- cumsum(numeric_pca$sdev^2) / sum(numeric_pca$sdev^2)
+plot(cumulative_variance, type = "b", xlab = "Number of Principal Components", ylab = "Cumulative Variance Explained")
+
+# look at the cumulative variance contained in the above
+
+traindata_pca = cbind.data.frame(traindata[, 1:8], numeric_pca$x, traindata$class)
+print(traindata_pca)
